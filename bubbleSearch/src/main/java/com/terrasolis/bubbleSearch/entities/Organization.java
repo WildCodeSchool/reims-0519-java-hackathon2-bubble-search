0c452f41e222af2b12bda978767d67fc0d729971
cecil millerioux <cecil.millerioux@gmail.com>
2019-09-19 14:34:58 +0200

    add entity content

--- bubbleSearch/src/main/java/com/terrasolis/bubbleSearch/entities/Organization.java
+++ bubbleSearch/src/main/java/com/terrasolis/bubbleSearch/entities/Organization.java
@@ -7,7 +7,51 @@ import javax.persistence.Id;
 
 @Entity
 public class Organization {
+    public Organization(){
+    }
+
+    public Organization(String name, String mission, String domain){
+        this.name = name;
+        this.mission = mission;
+        this.domain = domain;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private String mission;
+    private String domain;
+    
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMission() {
+		return mission;
+	}
+
+	public void setMission(String mission) {
+		this.mission = mission;
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
 }
