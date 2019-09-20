3368389a63b072d036e8d47ed4990ab550b8cac9
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-20 13:31:42 +0200

    ok

--- bubbleSearch/src/main/java/com/terrasolis/bubbleSearch/controllers/OrganizationController.java
+++ bubbleSearch/src/main/java/com/terrasolis/bubbleSearch/controllers/OrganizationController.java
@@ -1,10 +1,6 @@
 package com.terrasolis.bubbleSearch.controllers;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
 import java.util.List;
-
-import javax.validation.Valid;
 
 import com.terrasolis.bubbleSearch.entities.Organization;
 import com.terrasolis.bubbleSearch.repositories.OrganizationRepository;
@@ -12,27 +8,24 @@ import com.terrasolis.bubbleSearch.repositories.OrganizationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class OrganizationController {
+
     @Autowired
     private OrganizationRepository organizationRepository;
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String index(Model model) {
         List<String> organizations = organizationRepository.findDistinctDomain();
         model.addAttribute("organizations", organizations);
         return "index";
     }
     
     @GetMapping("/organizations")
-    public String organizations(Model model) {
+    public String browse(Model model) {
         List<Organization> organizations = organizationRepository.findAll();
         model.addAttribute("organizations", organizations);
         return "search";
@@ -45,20 +38,4 @@ public class OrganizationController {
     return "terrasolis";
     }
 
-    @PutMapping("/organizations/{id}")
-    public Organization edit(@Valid @ModelAttribute Organization organization) {
-        return organizationRepository.save(organization);
-    }
-
-    @PostMapping("/organizations")
-    public Organization add(@Valid @ModelAttribute Organization organization) {
-        return organizationRepository.save(organization);
-    }
-
-    @DeleteMapping("/organizations/{id}")
-    public void destroy(@PathVariable Long id) {
-        organizationRepository.delete(
-            organizationRepository.findById(id).get()
-        );
-    }
 }
